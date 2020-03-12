2956b4ba1e9ee408f81e0fe3ed251737415e2fc6
gray <shinjoohyung25@gmail.com>
2020-03-12 16:00:16 -0400

    change in build

--- src/main/java/com/javatoy/web/HelloController.java
+++ src/main/java/com/javatoy/web/HelloController.java
@@ -1,4 +1,12 @@
 package com.javatoy.web;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HelloController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
 }
