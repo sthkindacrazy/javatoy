7f8763344c2804b9630d9f217e5b6a9b5c2a50c1
gray <shinjoohyung25@gmail.com>
2020-03-12 17:10:44 -0400

    add dto

--- src/main/java/com/javatoy/web/dto/HelloResponseDto.java
+++ src/main/java/com/javatoy/web/dto/HelloResponseDto.java
@@ -1,4 +1,12 @@
 package com.javatoy.web.dto;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+
 public class HelloResponseDto {
+    private final String name;
+    private final int amount;
 }
