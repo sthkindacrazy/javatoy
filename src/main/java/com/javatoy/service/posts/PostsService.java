32032a82f106e6f1e1fd433dd10c99d80edf8845
gray <shinjoohyung25@gmail.com>
2020-03-14 15:14:27 -0400

    add service

---
+++ src/main/java/com/javatoy/service/posts/PostsService.java
@@ -0,0 +1,17 @@
+package com.javatoy.service.posts;
+
+import com.javatoy.domain.posts.PostsRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
