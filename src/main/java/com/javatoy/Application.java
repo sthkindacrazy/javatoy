d358e590eafe45071fcb98ab11abbea67aa1cefd
gray <shinjoohyung25@gmail.com>
2020-03-16 15:29:15 -0400

    add BaseTimeEntity

--- src/main/java/com/javatoy/Application.java
+++ src/main/java/com/javatoy/Application.java
@@ -2,7 +2,9 @@ package com.javatoy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
--- src/main/java/com/javatoy/domain/posts/Posts.java
+++ src/main/java/com/javatoy/domain/posts/Posts.java
@@ -1,5 +1,6 @@
 package com.javatoy.domain.posts;
 
+import com.javatoy.domain.BaseTimeEntity;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,7 +14,7 @@ import javax.persistence.Id;
 @Getter
 @NoArgsConstructor
 @Entity // JPA annotation
-public class Posts {
+public class Posts extends BaseTimeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
